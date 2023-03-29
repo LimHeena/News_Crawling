f4300679882b68f6d38ec0458f4801d62e3cbe42
Heen_A <104770890+LimHeena@users.noreply.github.com>
2023-03-29 16:56:28 +0900

    add skeleton code try2 0328
    첫번째 스켈레톤 코드에서 수정한 파일

--- news_db.py
+++ news_db.py
@@ -17,23 +17,36 @@ class NewsDB:
     생성시 데이터베이스(news_db) 접속, 소멸시 연결 해제.
     데이터 삽입 함수: insert_news(self, df)
     데이터 추출 함수: select_news(self, condition)
-    추출 시 condition에는 1. 기자(홍길동) 2. 날짜(2023-03-28) 3. (플랫폼, 대분류) 4. (플랫폼, 대분류, 소분류)
+    추출 시 condition에는 1. 기자(홍길동) 2. 날짜(2023-03-28) 3. 날짜 between(2023-03-03 2023-03-28)
+                         4. (플랫폼, 대분류) 5. (플랫폼, 대분류, 소분류) 
     """
 
-    def __init__(self) -> None:
+    def __init__(self, configs) -> None:
         """
         데이터베이스 접속
         인자 : 데이터베이스 접속정보
         """
-        global connection
-        connection = pymysql.connect()
+        
+        self.connection = pymysql.connect(**configs)
+        
+        tmp = [l.rstrip().split(',') for l in open('./main_category').readlines()]
+        self.MAIN_CATEGORY_DICT = {v: k for k, v in tmp}
+        tmp = [l.rstrip().split(',') for l in open('./sub_category').readlines()]
+        self.SUB_CATEGORY_DICT = {v: k for k, v in tmp}
+        tmp = [l.rstrip().split(',') for l in open('./platform_info').readlines()]
+        self.PLATFORM_DICT = {v: k for k, v in tmp}
 
     def __del__(self) -> None:
         """
         데이터베이스 연결 해제
         """
-        connection.close()
+        self.connection.close()
 
+    def clean_title(self, title):
+        pass
+    
+    def clean_content(self, content):
+        pass
 
     def insert_news(self, df):
         """
@@ -48,73 +61,50 @@ class NewsDB:
         execute 대신 execute_many 메서드로 한번에 삽입 (서버 DB 1GB 램 고려)
         """
         
+        if 'url' not in list(df.columns):
+            df['url'] = ""
+            
         df_cols = list(df.columns) 
         
-        with connection.cursor() as cursor:
+        with self.connection.cursor() as cursor:
+            cursor.execute('describe news')
             cols = cursor.fetchall()
             cols_list = []
             for col in cols:
                 cols_list.append(col[0])
                 
-
             if sorted(df_cols) != sorted(cols_list):
-                raise ValueError #에러 발생
+                raise ValueError('컬럼명 오류!') #에러 발생
 
                        
-        titles = []
-        for title in df['title']:
-            new_title = clean_title(title) #cleansing
-            titles.append(new_title)
-        df['title'] = titles
-        
-        contents = []
-        for content in df['content']:
-            new_content = clean_content(content) #cleansing
-            contents.append(new_content)
-        df['content'] = contents
+        df['title'] = df['title'].apply(clean_title) 
+        df['content'] = df['content'].apply(clean_content)
         
         
-        #insert SQL문
-        with connection.cursor() as cursor:
+        #Insert SQL문
+        with self.connection.cursor() as cursor:
             
-            #뉴스 테이블
-            sql = "INSERT INTO `news`(`news_id`, `title`, `writer`, `content`, `writed_at`, `url`) VALUES(%s, %s, %s, %s, %s, %s)"
+            #뉴스 테이블  ##다른 테이블은 한번 따로 넣어주는 것이 좋겠다고 판단(파일 갖고 있으니)
+            sql = "INSERT INTO `news`(`title`, `writer`, `content`, `writed_at`, `url`, `platform`,`main_category`,`sub_category`) VALUES(%s, %s, %s, %s, %s, %s, %s, %s)"
             mylist=[]
+            idx = 0
             
             for index, row in df.iterrows(): #데이터프레임 한 행씩 가져옴
-                mylist.append(tuple((int(row[0]), row[1], row[2], row[3], datetime.strptime(row[4], '%Y-%m-%d %H:%M:%S'), row[5])))
+                idx += 1         
+                mylist.append(tuple((row[0], row[1], row[2], datetime.strptime(row[3], '%Y-%m-%d %H:%M:%S'), row[4], row[5], row[6], row[7])))
+                
+                if idx == 10000: #데이터 10000개씩 넣기
+                    cursor.executemany(sql, mylist)
+                    idx = 0
+                    mylist.clear()
+                    
+                try:
+                    cursor.executemany(sql, mylist) #위에서 삽입되지 못한 것들 넣기
+                except:
+                    pass
             
-            cursor.executemany(sql, mylist)
-            
-            #서브카테고리 테이블
-            sql = "INSERT INTO `sub_category`(`sub_id`, `name`) VALUES(%s, %s)"
-            mylist=[]
-            
-            for index, row in df.iterrows(): #데이터프레임 한 행씩 가져옴
-                mylist.append(tuple((int(row[0]), row[1])))
-            
-            cursor.executemany(sql, mylist)
-            
-            #메인카테고리 테이블
-            sql = "INSERT INTO `main_category`(`main_id`, `name`) VALUES(%s, %s)"
-            mylist=[]
-            
-            for index, row in df.iterrows(): #데이터프레임 한 행씩 가져옴
-                mylist.append(tuple((int(row[0]), row[1])))
-            
-            cursor.executemany(sql, mylist)
-            
-            #플랫폼 테이블
-            sql = "INSERT INTO `platform_info`(`platform_id`, `name`) VALUES(%s, %s)"
-            mylist=[]
-            
-            for index, row in df.iterrows(): #데이터프레임 한 행씩 가져옴
-                mylist.append(tuple((int(row[0]), row[1])))
-            
-            cursor.executemany(sql, mylist)
-            
-            
-            connection.commit() 
+            self.connection.commit() 
+            print('모든 데이터 삽입 완료!')
 
 
     def select_news(self, condition): 
@@ -127,12 +117,26 @@ class NewsDB:
 
         DB 액세스를 줄이는 방법도 한번쯤 생각해보면 좋음 (캐싱이라는 개념, *여기서 구현해야하는것은 아님)
         """
-        with connection.cursor() as cursor:
+        
+        with self.connection.cursor() as cursor:
             sql_pre = "SELECT DISTINCT * FROM `news`"
             sql_post = ""
             
+            main_key_list = []
+            for key, value in self.MAIN_CATEGORY_DICT.items():
+                main_key_list.append(key)
+                
+            sub_key_list = []
+            for key, value in self.SUB_CATEGORY_DICT.items():
+                sub_key_list.append(key)
+            
+            plat_key_list = []
+            for key, value in self.PLATFORM_DICT.items():
+                plat_key_list.append(key) 
+                   
             cons = condition.split() #플랫폼, 대분류 (네이버 사회)
-            if cons[1] in ['정치','경제','사회','세계','IT/과학','스포츠','생활/문화','IT','문화','국제']:
+            
+            if cons[1] in main_key_list:
                 
                 sql_post = """ 
                 INNER JOIN `platform_info` 
@@ -141,10 +145,11 @@ class NewsDB:
                 INNER JOIN `main_category`
                 On news.main_id = main_category.main_id
                 WHERE `name` = '{}' 
+                LIMIT 10000;
                 """.format(cons[0], cons[1]) 
-            
+                
             cons = condition.split() #플랫폼, 대분류, 소분류 (네이버 사회 사건사고)
-            if cons[1] in ['정치','경제','사회','세계','IT/과학','스포츠','생활/문화','IT','문화','국제']:
+            if cons[2] in sub_key_list:
                 
                 sql_post = """ 
                 INNER JOIN `platform_info` 
@@ -156,18 +161,27 @@ class NewsDB:
                 INNER JOIN `sub_category`
                 On news.sub_id = sub_category.sub_id
                 WHERE `name` = '{}' 
+                LIMIT 10000;
                 """.format(cons[0], cons[1], cons[2]) 
                 
             if (cons[0] != '네이버') and (cons[0] != '다음'): #기자 이름 (홍길동)
                 sql_post = """
                 WHERE `writer` = '{}'
+                LIMIT 10000;
                 """.format(cons[0])
             
             if (len(cons[0]) == 10): #날짜 (2023-03-28)
                 sql_post = """
                 WHERE `writed_at` = '{}'
+                LIMIT 10000;
                 """.format(cons[0])
-
+                
+            if (len(cons[0]) == 23): #날짜 between (2023-03-03 2023-03-28)
+                sql_post = """
+                WHERE `writed_at` BETWEEN '{}' AND '{}'
+                LIMIT 10000;
+                """.format(cons[0], cons[1])
+            
             sql = sql_pre + sql_post
             cursor.execute(sql)
             result = cursor.fetchall()
@@ -176,6 +190,10 @@ class NewsDB:
 
 if __name__ == '__main__':
     # 테스트코드 작성
+    
+    with open('./config.txt', 'r') as f:
+        lines = [line.split('=') for line in f.readlines()]
+        configs = {i: j for i, j in lines}
     
     mydf = NewsDB() #객체 생성
     mydf.insert_news(df) #데이터베이스에 데이터 삽입
@@ -191,5 +209,8 @@ if __name__ == '__main__':
     
     one_date = NewsDB()
     one_date.select_news('2023-03-28') #날짜 데이터 가져오기 및 출력
+    
+    between_date = NewsDB()
+    between_date.select_news('2023-03-03 2023-03-28') #두 날짜 사이 데이터 가져오기 및 출력
     
     
